535ac299e40a9e65935d429a0ccd7b00cc1b2758
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-02-07 16:18:11 +0530

    refactor: change border radius and ring width

--- src/app/components/CategoryLink.tsx
+++ src/app/components/CategoryLink.tsx
@@ -10,7 +10,7 @@ export default function CategoryLink({
   return (
     <Link
       href={`/${category.toLowerCase()}`}
-      className="flex items-center gap-4 rounded-xl bg-white p-4 shadow-sm hover:bg-blue-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-grey-500 md:gap-8 lg:p-6"
+      className="flex items-center gap-4 rounded-md bg-white p-4 shadow-sm hover:bg-blue-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-grey-500 md:gap-8 md:rounded-lg lg:p-6"
     >
       {icon}
       <span className="text-sm leading-none md:text-xl">{category}</span>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ export default function Button({
   children,
 }: { children: React.ReactNode } & ComponentProps<"button">) {
   return (
-    <button className="w-full rounded-xl bg-purple-600 p-4 text-sm leading-none text-white shadow-md hover:bg-purple-600/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 md:p-8 md:text-xl">
+    <button className="w-full rounded-md bg-purple-600 p-4 text-sm leading-none text-white shadow-md hover:bg-purple-600/90 focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-500 md:rounded-lg md:p-8 md:text-xl">
       {children}
     </button>
   )
