9f813331a31d3a6af6339c200e4171800e41722e
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-02-11 22:40:40 +0530

    QuestionForm: remove unwanted state variables

--- src/app/[category]/[question]/components/AnswerOption.tsx
+++ src/app/[category]/[question]/components/AnswerOption.tsx
@@ -8,7 +8,7 @@ type AnswerOptionType = {
   labelLetter: string
   answer: { id: string; answer: string; questionId: string }
   isSelected: boolean
-  isAnswerCorrect: boolean | null
+  isUserAnswerCorrect: boolean | undefined
   isCorrectAnswer: boolean
 }
 
@@ -17,7 +17,7 @@ export default function AnswerOption({
   labelLetter,
   answer,
   isSelected,
-  isAnswerCorrect,
+  isUserAnswerCorrect,
   isCorrectAnswer,
 }: AnswerOptionType) {
   return (
@@ -25,20 +25,16 @@ export default function AnswerOption({
       value={answer.id}
       className={twMerge(
         "group flex w-full cursor-pointer items-center gap-4 rounded-md bg-white p-4 shadow-sm ring-purple-600 data-[disabled]:cursor-not-allowed data-[focus-visible]:ring data-[selected]:ring md:gap-8 md:rounded-lg lg:p-6",
-        isSelected && isAnswerCorrect && "ring ring-green-500",
-        isSelected && !isAnswerCorrect && "ring ring-red"
+        isSelected && isUserAnswerCorrect && "ring ring-green-500",
+        isSelected && !isUserAnswerCorrect && "ring ring-red"
       )}
     >
       <AnswerLetter
         letter={labelLetter}
         className={twMerge(
           "group-data-[selected]:bg-purple-600 group-data-[selected]:text-white",
-          isSelected &&
-            isAnswerCorrect &&
-            "text-white group-data-[selected]:bg-green-500",
-          isSelected &&
-            !isAnswerCorrect &&
-            "text-white group-data-[selected]:bg-red"
+          isSelected && isUserAnswerCorrect && "bg-green-500 text-white",
+          isSelected && !isUserAnswerCorrect && "bg-red text-white"
         )}
       />
       <p className="text-sm leading-none md:text-xl">{answer.answer}</p>
